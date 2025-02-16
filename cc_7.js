5a8c727d48f73a8d74e68410c24966b17408b593
henriquesenaribeiro <henriqueribeiro@usf.edu>
2025-02-16 13:33:29 -0500

    Task 1: Customer Invoice Calculation

---
+++ cc_7.js
@@ -0,0 +1,10 @@
+// Task 1: Customer Invoice Calculation
+
+function calculateInvoice(subtotal, taxRate, discount) {
+    let total = (subtotal + (subtotal * taxRate)) - discount;
+    return `Total Invoice: $${total.toFixed(2)}`;
+};
+console.log(calculateInvoice(100, 0.08, 5)); // Expected: "Total Invoice: $103.00"
+console.log(calculateInvoice(500, 0.1, 20)); // Expected: "Total Invoice: $530.00"
+
+// Created a function that takes three parameters and implemented a formula to compute an invoice, then returned sample test data.
